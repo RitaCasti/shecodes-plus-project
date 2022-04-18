0128cafc52cc5a8cc68c428cb797cec1ddc22137
RitaCasti <102758430+RitaCasti@users.noreply.github.com>
2022-04-18 21:42:56 +0100

    add forecast

--- src/app.js
+++ src/app.js
@@ -28,6 +28,45 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
+  let forecastHTML = `<div class = "row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+               <div class="weather-forecast-date">${day}</div>
+               <img 
+                 src=""
+                 alt= "" 
+                 width=36px;
+                 />
+                 <div class="weather-forecast-temperature">
+                   <span class = "weather-forecast-temperature-max"
+                   > 18º </span>
+                   <span class = "weather-forecast-temperature-min"
+                   > 12º </span>
+                 </div>
+           </div>
+           `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "754e983504b813887227b74c34ced100";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -49,6 +88,7 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "754e983504b813887227b74c34ced100";
