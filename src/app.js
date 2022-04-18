424f61b81d821b4c69955d07610426c5ac04a810
RitaCasti <102758430+RitaCasti@users.noreply.github.com>
2022-04-18 11:43:47 +0100

    time

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = timestamp.getHours();
+   if (hours < 10) {
+   hours = `0{hours}`;
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0{minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -11,9 +34,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   console.log(response.data);
+  dateElement = formatDate(response.data.dt * 1000);
 }
-
 let apiKey = "754e983504b813887227b74c34ced100";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
